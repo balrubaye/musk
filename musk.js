64272cbf68baf4d9cdea2363bbddee8f1b918d3c
balrubaye <bassam.rubaye@gmail.com>
2016-06-27 14:45:34 -0700

    Add mixin() to extend object

--- musk.js
+++ musk.js
@@ -35,8 +35,46 @@
 
 	musk.hasProtoProperty= hasPrototypeProperty;
 
-	
 
+	//check if valid Array type
+	function isArray(arr){
+		return Object.prototype.toString.call(arr) === '[object Array]';
+	}
+
+	musk.isArray= isArray;
+
+	//Extends a receiver object from one or more suppliers
+	function mixin(receiver, supplier){
+		var supArray =[];
+
+		//check if the second argument is one supplier or array of 
+		if(isArray(supplier)) {supArray=supplier; }
+		
+		else{ supArray.push(supplier); }
+
+		//if ES5 and above
+		if( typeof Object.keys !== 'undefiend'){
+
+			supArray.forEach(function(supp){
+				Object.keys(supp).forEach(function(prop){
+							receiver[prop]= supp[prop];
+				});
+			});
+		}
+
+		else{
+			supArray.forEach(function(supp){
+				for( prop in supp){
+					if(Object.hasOwnProperty.call(supp, prop)){
+					receiver[prop]= supp[prop];
+				   }
+				}
+			});
+		}
+
+	}
+
+	musk.mixin = mixin ;
 
 
 }).call(this)
